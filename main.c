24f0b2eac849c87dde33ac90cd2f56a253b47b43
Sergey Stepanov <serg_admin@rambler.ru>
2016-01-06 17:54:10 +0600

    Реализованна функция вывода строки символов в указанную позицию дисплея.

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-//#define F_CPU 8000000UL
-//#include <util/delay.h>
+#include <tools/uart_async.h>
 
 #define PORT_DATA PORTD
 #define PORT_OTHER PORTC
@@ -10,6 +9,20 @@
 #define PIN_RS PORTC0
 #define PIN_RW PORTC3
 #define PIN_E PORTC2
+
+#define J204A_CLEAR_DISPLAY j204a_applay(0, 0, 0b00000001)
+#define J204A_CURSOR_TO_HOME j204a_applay(0, 0, 0b00000010)
+#define J204A_PRINT(c) j204a_applay(1, 0, c)
+#define J204A_WIDTH 20
+
+#define J204A_PRINT_HEX(b) j204a_applay(1, 0, halfchar_to_hex(b >> 4));\
+    j204a_applay(1, 0, halfchar_to_hex(b & 0x0F))
+    
+#define BUTTON0 0b11111110
+#define BUTTON1 0b11111101
+#define BUTTON2 0b11111011
+
+uint8_t j204a_ram[4][J204A_WIDTH];
 
 void _delay_ms(uint16_t ms) {
   uint16_t delay;
@@ -21,6 +34,60 @@ void _delay_ms(uint16_t ms) {
   }
 }
 
+void _delay_us(uint16_t us) {
+  uint16_t delay;
+  for (;us; us--) {
+    delay = 2;
+    for(; delay; delay--){
+      asm("");
+    }
+  }
+}
+
+void j204a_applay(uint8_t rs, uint8_t rw, uint8_t data) {
+    // Приводим шину данных в требующееся состояние
+    // и ждем 25 микросекунд.
+    if (rs) PORT_OTHER |= _BV(PIN_RS);
+    else PORT_OTHER &= ~(_BV(PIN_RS));
+    if (rw) PORT_OTHER |= _BV(PIN_RW);
+    else PORT_OTHER &= ~(_BV(PIN_RW));
+    PORT_DATA = data ;
+    PORT_OTHER |= _BV(PIN_E); // Старт транзакции
+    _delay_us(25);
+    
+    // Отмечаем завершение транзакции и ждем еще 25 микросекунд.
+    PORT_DATA &= ~(_BV(PORT7));
+    PORT_OTHER &= ~(_BV(PIN_E));
+    _delay_us(25);
+}
+
+void j204a_refresh(void) {
+  uint8_t line = 0;
+  uint8_t col;
+  J204A_CURSOR_TO_HOME;
+  _delay_ms(2);
+  while(line < 4) {
+    for(col = 0; col < J204A_WIDTH; col++) {
+      J204A_PRINT(j204a_ram[line][col]);
+    }
+    switch(line) {
+      case 0 : line = 2; break;
+      case 1 : line = 3; break;
+      case 2 : line = 1; break;
+      case 3 : line = 4; break;
+    }
+  }
+}
+
+void j204a_clear(void) {
+  uint8_t* ram = (uint8_t*)j204a_ram;
+  uint8_t n;
+  J204A_CLEAR_DISPLAY;
+  for(n = 0; n < (J204A_WIDTH * 4); n++) {
+    ram[n] = 0x20;
+  }
+}
+/*
 uint8_t halfchar_to_hex(uint8_t tt) {
   if (tt < 10) return tt + 48;
   switch (tt) {
@@ -33,68 +100,44 @@ uint8_t halfchar_to_hex(uint8_t tt) {
   }
   return 'X';
 }
+*/
 
-void j204a_applay(uint8_t rs, uint8_t rw, uint8_t data) {
-    PORT_OTHER |= _BV(PIN_E);
-    if (rs) PORT_OTHER |= _BV(PIN_RS);
-    else PORT_OTHER &= ~(_BV(PIN_RS));
-    if (rw) PORT_OTHER |= _BV(PIN_RW);
-    else PORT_OTHER &= ~(_BV(PIN_RW));
-    PORT_DATA = data ;
-    _delay_ms(1);
-    PORT_DATA &= ~(_BV(PORT7));;
-    PORT_OTHER &= ~(_BV(PIN_E));
-    _delay_ms(1);
-}
-
-int main(void) {
-  sei();
-  uint8_t tmp = 0;
-  DDRD = 0xFF;
-  DDRC = 0xFF;
-  PORTA = 0xFF;
-  _delay_ms(500);
-  PORT_DATA = 0;
-
-  j204a_applay(0, 0, 0b00001100); // Настройка 1DCB
-  //j204a_applay(0, 0, 0b00111100); // Настройка 1DCB
-  //j204a_applay(0, 0, 0b10000000); // set ddram address
-  j204a_applay(0, 0, 0b10000000); // set ddram address
-  
-  j204a_applay(0, 0, 0b01001000); // set CGRAM address
-  
-  j204a_applay(1, 0, 0b00011111);  
-  j204a_applay(1, 0, 0b00010001);  
-  j204a_applay(1, 0, 0b00010101);  
-  j204a_applay(1, 0, 0b00010101);  
-  j204a_applay(1, 0, 0b00010101);  
-  j204a_applay(1, 0, 0b00010101);  
-  j204a_applay(1, 0, 0b00010001);  
-  j204a_applay(1, 0, 0b00011111);  
-  
-  j204a_applay(0, 0, 0b00000010); // Курсор домой
-  for (;;) {
-    j204a_applay(1, 0, tmp);
-    j204a_applay(1, 0, halfchar_to_hex(tmp >> 4));
-    j204a_applay(1, 0, halfchar_to_hex(tmp & 0x0F));
-    tmp++;
-
-
-    if (((~tmp) & 0b00000111) == 0x07) {
-      j204a_applay(0, 0, 0b00000010); // Курсор домой
-      _delay_ms(500);
-      while (PINA == 0xFF); 
-      switch(PINA) {
-        case 0b11111110 :
-          break;
-        case 0b11111101 :
-          tmp = tmp - 8;
-          break;
-        case 0b11111011 :
-          tmp = tmp - 16;
-          break;
-      }
-      j204a_applay(0, 0, 0b00000001); // set ddram address  
+void j204aPrint(uint8_t col, uint8_t line, char* str){
+  uint8_t n = 0;
+  while(str[n]) {
+    j204a_ram[line][col++] = str[n++];
+    if (col >= J204A_WIDTH) {
+      col = 0;
+      line++;
     }
   }
+}
+
+void j204aInit(void){
+  DDRD = 0xFF;  // Data пины
+  DDRC = 0xFF;  // Управляющие пины  
+  PORT_DATA = 0;
+  _delay_ms(20); // Даем дисплею время включиться
+  j204a_applay(0, 0, 0b00001100); // Настройка 1,D,C,B
+  _delay_ms(2);
+  j204a_applay(0, 0, 0b00010100); 
+  _delay_ms(2);
+   // Настройка 1,DL,N,F,*,*
+   //    DL - 1 : 8-ми проводная шина.
+   //         0 : 4-х проводная шина.
+   //     N - 1 : Черезстрочный ?
+   //         0 : Однострочный
+   //     F - 1 : матрица символа 5x10
+   //         0 : матрица символа 5x7 
+  j204a_applay(0, 0, 0b00111000); 
+  _delay_ms(2);
+  j204a_clear();
+}
+int main(void) {
+  //PORTA = 0xFF; // Кнопки
+  j204aInit();
+  
+  j204aPrint(4, 2, "Hello World.");
+  
+  j204a_refresh();
 }
